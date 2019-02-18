f21bf17263d6ed439660790e9c444629e47534a7
Hakan ERSU <hakanersu@gmail.com>
2019-02-18 14:29:25 +0300

    Reseller login added

--- src/Api/Reseller.php
+++ src/Api/Reseller.php
@@ -8,4 +8,12 @@ class Reseller extends Base
     {
         return $this->request('resellers/details.json', ['reseller-id' => $id], 'GET', false);
     }
+
+    public function loginToken($id, $ip)
+    {
+        return $this->request('resellers/generate-login-token.json', [
+            'reseller-id' => $id,
+            'ip' => $ip,
+        ], 'GET', false);
+    }
 }
